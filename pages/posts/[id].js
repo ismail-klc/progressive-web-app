114de5747456f42c3b530794d6a3e199f9c5a382
İsmail Kılıç <152120171049@ogrenci.ogu.edu.tr>
2021-07-16 02:43:28 +0300

    post detail edited

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,30 +1,57 @@
 import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
-import { useRecoilState } from 'recoil';
+import { selectorFamily, useRecoilValue } from 'recoil';
 import { postsState } from '../../states/posts';
 import Comment from '../../components/comment';
 import axios from 'axios';
 
-function PostDetail() {
-    const router = useRouter();
-    const id = router.query.id;
-    const [posts, setPosts] = useRecoilState(postsState);
+const getPostState = selectorFamily({
+    key: 'getPostState',
+    get: (id) => ({ get }) => {
+        const posts = get(postsState);
+        return posts.find((str) => {
+            return str.id === parseInt(id);
+        });
+    },
+});
+
+function PostDetail({ id }) {
+    const router = useRouter()
+
+    const [post, setPost] = useState(useRecoilValue(getPostState(id)));
     const [comments, setComments] = useState([]);
     const url = `https://jsonplaceholder.typicode.com/comments?postId=${id}`
 
     useEffect(() => {
+        console.log(post);
+        if (post === undefined || post.length === 0) {
+            getPost();
+        }
         getComments();
-    }, [])
+    }, [post, id])
+
+    const getPost = async () => {
+        const res = await axios.get(`https://jsonplaceholder.typicode.com/posts?id=${id}`);
+        setPost(res.data[0]);
+    }
 
     const getComments = async () => {
         const res = await axios.get(url);
         setComments(res.data);
     }
 
+    if (router.isFallback) {
+        return <div className="container">Loading...</div>
+    }
+
 
     return (
         <div className="container">
-            <h2>Comments</h2>
+            <h3 style={{ fontWeight: 'bold' }}>{post && post.title}</h3>
+            <h3>{post && post.body}</h3>
+            <br></br>
+
+            <h4>Comments</h4>
             {
                 comments.length > 0 ?
                     comments.map(c => (
@@ -40,6 +67,23 @@ function PostDetail() {
             }
         </div>
     )
+}
+
+export async function getStaticPaths() {
+    return {
+        paths: [
+            { params: { id: '*' } }
+        ],
+        fallback: true
+    };
+}
+
+export async function getStaticProps({ params }) {
+    return {
+        props: {
+            id: params.id
+        },
+    }
 }
 
 export default PostDetail
